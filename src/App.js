10eada1c0f21defbebf9cca436840832c47191d4
Aswin J <aswinj271@gmail.com>
2024-12-31 12:02:40 +0530

    Todo App Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import TodoApp from './component/TodoApp/TodoApp'
 
 
 
 export default class App extends Component {
   render() {
     return (
-      <div>Todo App</div>
+      <div>
+        <TodoApp/>
+      </div>
     )
   }
 }
